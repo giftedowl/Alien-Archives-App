6aab2811ae1faaaac58dada01aa1f4f50ead3f2d
John Lane <info@digitalumbrellamedia.com>
2024-11-16 11:11:02 -0500

    Initial setup

---
+++ Aliens Archive/Aliens_ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aliens_ArchiveApp.swift
+//  Aliens Archive
+//
+//  Created by John Lane on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Aliens_ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
