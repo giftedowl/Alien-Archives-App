81b0b20ec3379da180067b03d0e46e387e1be3cd
Gifted Owl <giftedowl@gmail.com>
2025-01-15 21:14:14 -0500

    Adding Firebase for Crash Reporting and Analytics

--- Aliens Archive/AliensArchive.swift
+++ Aliens Archive/AliensArchive.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct AliensArchive: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             SpeciesView()
